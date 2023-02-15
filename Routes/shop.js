7024c54090a953a7e84e8c676967d77b43be69d4
hafiz.zeeshan <hafiz.zeeshan@piecyfer.com>
2023-02-15 18:37:46 +0500

    Working with EJS and Working on the Layout with Partials

--- Routes/shop.js
+++ Routes/shop.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
     // res.send(adminData.products)
     // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
     const products = adminData.products
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: "/"});
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: "/", hasProducts: products.length>0, activeShop: true, productCss: true});
 });
 
 module.exports = router
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express")
 const app = express()
 
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminData = require("./Routes/admin")

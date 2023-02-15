0a08be98fa7f4be98f0dacc216f8affeee973838
hafiz.zeeshan <hafiz.zeeshan@piecyfer.com>
2023-02-15 13:02:16 +0500

    Finishing the Pug Template by add active class on navbar and dynamic page title

--- Routes/shop.js
+++ Routes/shop.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
     // res.send(adminData.products)
     // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
     const products = adminData.products
-    res.render('shop', {prods: products, docTitle: 'Shop'});
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: "/"});
 });
 
 module.exports = router

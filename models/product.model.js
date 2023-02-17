1e5742ae5989ecf7f3c63a1344cb8d37839778fb
hafiz.zeeshan <hafiz.zeeshan@piecyfer.com>
2023-02-17 09:47:48 +0500

    Displaying Product Data as the entered product data from user

--- models/product.model.js
+++ models/product.model.js
@@ -13,11 +13,11 @@ const getProductsFromFile = (cb) => {
     })
 }
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
+    constructor(title, imageUrl, price, description) {
         this.title = title;
         this.imageUrl = imageUrl;
+        this.price = price;
         this.description = description;
-        this.price = price
 
     }
 

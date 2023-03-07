4c198c00b2eed0da854cbabacdd387c055ed9c83
hafiz.zeeshan <hafiz.zeeshan@piecyfer.com>
2023-03-07 13:06:01 +0500

    Inserting Data & Creating a Product

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -40,12 +40,16 @@ exports.postAddProducts = (req, res, next) => {
     const price = req.body.price
     const description = req.body.description
 
-    const product = new Product(null, title, imageUrl, price, description)
-    product.save()
-        .then(() => {
-            res.redirect('/');
+    Product.create({
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description
+    })
+        .then(result => {
+            console.log("Product created successfully!")
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err)
         })
 }
--- main.js
+++ main.js
@@ -24,7 +24,6 @@ app.use(errorController.get404)
 
 sequelize.sync()
     .then(result => {
-        console.log(result)
         app.listen(8081)
     })
     .catch(err => {

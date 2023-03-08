acc8a8d0ce0f556d1ff15e286cc4eab1bc80bd78
hafiz.zeeshan <hafiz.zeeshan@piecyfer.com>
2023-03-08 18:35:12 +0500

    Creating a User Model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const {DataTypes} = require("sequelize")
+
+const sequenlize = require('../util/database')
+
+const User = sequenlize.define('user',{
+    id:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+module.exports = User
